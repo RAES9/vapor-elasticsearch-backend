03b543c3a2265b2a03d57c35b3b1de738c3249b3
Edwin Rivas <geko@Edwins-Mac-mini.local>
2019-12-23 16:22:04 -0600

    ok

---
+++ Sources/App/Controllers/RecipeFinderController.swift
@@ -0,0 +1,113 @@
+/// Copyright (c) 2019 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Vapor
+
+final class RecipeFinderController {
+  // TODO: Add more here ...
+    let elasticClient: ElasticsearchClient
+    init(elasticClient: ElasticsearchClient) {
+      self.elasticClient = elasticClient
+    }
+    
+}
+extension RecipeFinderController: RouteCollection {
+  func boot(router: Router) throws {
+    // TODO: Add more here ...
+    let routes = router.grouped("api", "recipes")
+    routes.post(Recipe.self, use: createHandler)
+    routes.get(String.parameter, use: getSingleHandler)
+    routes.put(Recipe.self, at: String.parameter, use: updateHandler)
+    routes.get(use: getAllHandler)
+    routes.delete(String.parameter, use: deleteHandler)
+    routes.get("search", use: searchHandler)
+  }
+    
+  func createHandler(_ req: Request, recipe: Recipe) throws ->
+      Future<Recipe> {
+      return try elasticClient
+        .createDocument(recipe, in: "recipes", on: req)
+        .map { response in
+      recipe.id = response.id
+      return recipe
+    }
+  }
+  func getSingleHandler(_ req: Request) throws -> Future<Recipe> {
+      let id: String = try req.parameters.next()
+      return try elasticClient
+        .getDocument(id: id, from: "recipes", on: req)
+        .map { (response: ESGetSingleDocumentResponse<Recipe>) in
+          // 3
+          let recipe = response.source
+          recipe.id = response.id
+          return recipe
+      }
+   }
+  func updateHandler(_ req: Request, recipe: Recipe) throws ->
+      Future<Recipe> {
+      let id: String = try req.parameters.next()
+      return try elasticClient.updateDocument(recipe, id: id,
+        in: "recipes", on: req).map { response in
+        return recipe
+      }
+   }
+    func getAllHandler(_ req: Request) throws -> Future<[Recipe]> {
+      return try elasticClient
+        .getAllDocuments(from: "recipes", on: req)
+        .map { (response: ESGetMultipleDocumentsResponse<Recipe>) in
+          return response.hits.hits.map { doc in
+            let recipe = doc.source
+            recipe.id = doc.id
+            return recipe
+        }
+      }
+    }
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+      let id: String = try req.parameters.next()
+      return try elasticClient.deleteDocument(id: id, from: "recipes",
+        on: req).map { response in
+        return .ok
+      }
+    }
+    func searchHandler(_ req: Request) throws -> Future<[Recipe]> {
+      guard let term = req.query[String.self, at: "term"] else {
+        throw Abort(.badRequest, reason: "`term` is mandatory")
+      }
+      let searchTerm = "(name:*\(term)* OR description:*\(term)* OR ingredients:*\(term)*)"
+      return try elasticClient
+        .searchDocuments(from: "recipes", searchTerm: searchTerm,
+          on: req)
+        .map { (response: ESGetMultipleDocumentsResponse<Recipe>) in
+        return response.hits.hits.map { doc in
+          let recipe = doc.source
+          recipe.id = doc.id
+          return recipe
+        }
+      }
+    }
+}
---
+++ Sources/App/Services/Elasticsearch/ElasticsearchClient.swift
@@ -0,0 +1,157 @@
+/// Copyright (c) 2019 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Vapor
+import HTTP
+
+// MARK: Client
+public final class ElasticsearchClient {
+
+  private let scheme: String
+  private let host: String
+  private let port: Int
+  private let username: String?
+  private let password: String?
+
+  public init(scheme: String = "http", host: String, port: Int = 9200, username: String? = nil, password: String? = nil) {
+    self.scheme = scheme
+    self.host = host
+    self.port = port
+    self.username = username
+    self.password = password
+  }
+
+  private func sendRequest<D: Decodable>(_ request: HTTPRequest, to url: URLRepresentable, on container: Container) throws -> Future<D> {
+    print("\nRequest:\n\n\(request)\n")
+    return try sendRequest(request, to: url, on: container).flatMap { response in
+      print("\nResponse:\n\n\(response)\n")
+      switch response.http.status.code {
+      case 200...299: return try ElasticsearchClient.jsonDecoder.decode(D.self, from: response.http, maxSize: 1_000_000, on: container)
+      default: throw Abort(response.http.status)
+      }
+    }
+  }
+
+  private func sendRequest(_ request: HTTPRequest, to url: URLRepresentable, on container: Container) throws -> Future<Response> {
+    return try container.client().send(
+      request.method,
+      headers: request.headers,
+      to: url,
+      beforeSend: { req in
+        req.http.body = request.body
+    })
+  }
+}
+
+// MARK: - Helper
+extension ElasticsearchClient {
+
+  private func baseURL(path: String, queryItems: [URLQueryItem] = []) -> URL {
+    var urlComponents = URLComponents()
+    urlComponents.scheme = scheme
+    urlComponents.host = host
+    urlComponents.port = port
+    urlComponents.path = path
+    urlComponents.queryItems = queryItems
+
+    guard let url = urlComponents.url else {
+      fatalError("malformed url: \(urlComponents)")
+    }
+    return url
+  }
+
+  private static let jsonDecoder: JSONDecoder = {
+    let jsonDecoder = JSONDecoder()
+    return jsonDecoder
+  }()
+
+  private static let jsonEncoder: JSONEncoder = {
+    let jsonEncoder = JSONEncoder()
+    return jsonEncoder
+  }()
+}
+
+// MARK: Requests
+extension ElasticsearchClient {
+
+  public func createDocument<Document: Encodable>(_ document: Document, in indexName: String, on container: Container) throws -> Future<ESCreateDocumentResponse> {
+    // TODO: Add more here ...
+    let url = baseURL(path: "/\(indexName)/_doc")
+    var request = try HTTPRequest(method: .POST, url: url, body:
+    ElasticsearchClient.jsonEncoder.encode(document))
+    request.contentType = .json
+    return try! sendRequest(request, to: url, on: container)
+  }
+
+  public func getDocument<Document: Decodable>(id: String, from indexName: String, on container: Container) throws -> Future<ESGetSingleDocumentResponse<Document>> {
+    // TODO: Add more here ...
+    let url = baseURL(path: "/\(indexName)/_doc/\(id)")
+    let request = HTTPRequest(method: .GET, url: url)
+    return try sendRequest(request, to: url, on: container)
+  }
+
+  public func getAllDocuments<Document: Decodable>(from indexName: String, on container: Container) throws -> Future<ESGetMultipleDocumentsResponse<Document>> {
+    // TODO: Add more here ...
+    let url = baseURL(path: "/\(indexName)/_search")
+    let request = HTTPRequest(method: .GET, url: url)
+    return try sendRequest(request, to: url, on: container)
+
+  }
+
+  public func searchDocuments<Document: Decodable>(from indexName: String, searchTerm: String, on container: Container) throws -> Future<ESGetMultipleDocumentsResponse<Document>> {
+    // TODO: Add more here ...
+    let url = baseURL(
+      path: "/\(indexName)/_search",
+      queryItems: [URLQueryItem(name: "q", value: searchTerm)]
+    )
+    let request = HTTPRequest(method: .GET, url: url)
+    return try sendRequest(request, to: url, on: container)
+  }
+
+  public func updateDocument<Document: Encodable>(_ document: Document, id: String, in indexName: String, on container: Container) throws -> Future<ESUpdateDocumentResponse> {
+    // TODO: Add more here ...
+    let url = baseURL(path: "/\(indexName)/_doc/\(id)")
+    var request = try HTTPRequest(method: .PUT, url: url, body:
+      ElasticsearchClient.jsonEncoder.encode(document))
+    request.contentType = .json
+    return try sendRequest(request, to: url, on: container)
+
+  }
+
+  public func deleteDocument(id: String, from indexName: String, on container: Container) throws -> Future<ESDeleteDocumentResponse> {
+    // TODO: Add more here ...
+    let url = baseURL(path: "/\(indexName)/_doc/\(id)")
+    let request = HTTPRequest(method: .DELETE, url: url)
+    return try sendRequest(request, to: url, on: container)
+  }
+
+  public func deleteIndex(_ indexName: String, on container: Container) throws -> Future<HTTPStatus> {
+    // TODO: Add more here ...
+    fatalError()
+  }
+}
